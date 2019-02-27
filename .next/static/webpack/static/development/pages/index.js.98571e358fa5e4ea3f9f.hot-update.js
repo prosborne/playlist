3c3bf788881e287e93c8d846a2fbb21b3b532493
Peter Riley Osborne <posborne@cbtnuggets.com>
2019-02-27 08:28:26 -0800

    added backend to site

---
+++ .next/static/webpack/static/development/pages/index.js.98571e358fa5e4ea3f9f.hot-update.js
@@ -0,0 +1,42 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./node_modules/react/index.js":
+false,
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony default export */ __webpack_exports__["default"] = (Index);
+    (function (Component, route) {
+      if(!Component) return
+      if (false) {}
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/")
+  
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ }),
+
+/***/ "dll-reference dll_831a3634f66cb1dada0c":
+false
+
+})
+//# sourceMappingURL=index.js.98571e358fa5e4ea3f9f.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+
+const Index = ()=> 
+    <div>
+        <h1>Plalist</h1>
+    </div>;
+
+
+export default Index;
